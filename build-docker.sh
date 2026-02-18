5ea7e1a8696377666c220f6add995642709d02bf
virmon <virmontumulak@gmail.com>
2026-02-18 16:45:32 +0800

    chore: update deployment config

--- build-docker.sh
+++ build-docker.sh
@@ -6,7 +6,7 @@
 # 1. Update the URL to use Artifact Registry in the US
 # Format: [REGION]-docker.pkg.dev/[PROJECT]/[REPO]/[IMAGE]
 # Replace 'my-repo' with your actual repository name
-IMAGE_URL="us-central1-docker.pkg.dev/$1/lmm-repo-prod/vendure"
+IMAGE_URL="us-central1-docker.pkg.dev/$1/lmm-repo-prod/vendure:latest"
 
 docker build -f apps/server/Dockerfile -t $IMAGE_URL .
 
