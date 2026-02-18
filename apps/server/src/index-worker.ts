a3e7fb0d47d57a07e37715cc1e105c6b72af814b
virmon <virmontumulak@gmail.com>
2026-02-18 15:24:46 +0800

    chore: updated index-worker.ts

--- apps/server/src/index-worker.ts
+++ apps/server/src/index-worker.ts
@@ -1,8 +1,20 @@
 import { bootstrapWorker } from '@vendure/core';
 import { config } from './vendure-config';
+import http from 'http';
 
 bootstrapWorker(config)
-    .then(worker => worker.startJobQueue())
+    .then(worker => {
+        worker.startJobQueue();
+        // START A DUMMY SERVER FOR CLOUD RUN HEALTH CHECKS
+        const port = process.env.PORT || 3000;
+        http.createServer((req, res) => {
+            res.writeHead(200);
+            res.end('Worker is healthy');
+        }).listen(port);
+
+        console.log(`Worker health check listening on port ${port}`);
+    })
     .catch(err => {
         console.log(err);
+        process.exit(1);
     });
