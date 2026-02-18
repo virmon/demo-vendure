ce58948cdf12762b5e806a7e5a4f78a9c5591b2f
virmon <virmontumulak@gmail.com>
2026-02-18 15:47:46 +0800

    chore: updated Dockerfile

--- apps/server/src/index-worker.ts
+++ apps/server/src/index-worker.ts
@@ -1,20 +1,8 @@
 import { bootstrapWorker } from '@vendure/core';
 import { config } from './vendure-config';
-import http from 'http';
 
 bootstrapWorker(config)
-    .then(worker => {
-        worker.startJobQueue();
-        // START A DUMMY SERVER FOR CLOUD RUN HEALTH CHECKS
-        const port = process.env.PORT || 3000;
-        http.createServer((req, res) => {
-            res.writeHead(200);
-            res.end('Worker is healthy');
-        }).listen(port);
-
-        console.log(`Worker health check listening on port ${port}`);
-    })
+    .then(worker => worker.startJobQueue())
     .catch(err => {
         console.log(err);
-        process.exit(1);
     });
